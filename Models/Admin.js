730a36ba419b508f228ddde61de9ae7882013357
Adejumo <excellencejumo@gmail.com>
2022-05-24 16:01:50 +0100

    update

--- Models/Admin.js
+++ Models/Admin.js
@@ -4,3 +4,4 @@ class Admin{
     }
 
 }
+module.exports=Admin;
---
+++ Models/Wallet.js
@@ -0,0 +1,21 @@
+const Withdraw = require('./Withdraw.js')
+const Deposit = require('./Deposit.js')
+const Transfer = require('./Transfer.js')
+class Wallet{
+    constructor(id,walletName,currencyName){
+        this.id=id;
+        this.walletName=walletName;
+        this.currencyName=currencyName;
+    }
+    //balance
+
+    //transactions
+
+    //withdraw
+
+    //deposit
+
+    //transfer
+
+}
+module.exports=Wallet;
