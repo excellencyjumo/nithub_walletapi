38b7fdc29a1019b1481d549795ed2fdfd44a72a6
Adejumo <excellencejumo@gmail.com>
2022-05-24 15:59:27 +0100

    update

---
+++ Models/User.js
@@ -0,0 +1,11 @@
+const Wallet = require('Wallet')
+class User{
+    constructor(userId,fName,lName,,email){
+        this.userId=userId;
+        this.fName=fName; // firstname
+        this.lName=LName; // lastname
+        this.email=email;
+        this.dor=new Date().toDateString(); //date of registration 
+    }
+    
+}
