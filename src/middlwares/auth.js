814ebb3a09b1e95843e99428e37fdedcd82fc4bf
tdadadavid <davidtofunmidada@gmail.com>
2022-07-02 19:24:20 +0100

    refactored code

--- src/middlwares/auth.js
+++ src/middlwares/auth.js
@@ -1,31 +1,25 @@
 const {sendResponse, verifyAuthToken} = require("../utils/utils");
 const User = require("../Components/Users/model/User");
+const logger = require("../config/winston");
 
 async function auth (req, res, next){
     try{
-
+        
         const token = req.header('x-auth-token');
-        if (!token) {
-            sendResponse(res, 401, "Access denied", {});
-            return;
-        }
+        if (!token) return sendResponse(res, 401, "Access denied");
 
         const { id } = await verifyAuthToken(token);
 
         const user = await User.findById(id);
-
-        if (!user){
-            sendResponse(res, 403, "Unable to perform action", {});
-            return;
-        }
+        if (!user) return sendResponse(res, 403, "Unable to perform action");
 
         req.user = user[0];
 
         next();
 
-
-    }catch (err){
-        sendResponse(res, 422, "Invalid token provided", {});
+    }catch (e){
+        logger.error(e.message, e);
+        sendResponse(res, 422, "Invalid token provided");
     }
 
 }
